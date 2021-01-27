0fe288f64ee77d5f040ae04c904d41b70ce6e38b
jtotoole <jtotoole@gmail.com>
2021-01-27 15:03:12 -0400

    dump schema file during build

--- apps/postgresql-server/bin/postgresql_server.sh
+++ apps/postgresql-server/bin/postgresql_server.sh
@@ -11,19 +11,17 @@ MC_POSTGRESQL_CONF_PATH="/etc/postgresql/11/main/postgresql.conf"
 /opt/mediacloud/bin/update_memory_config.sh
 
 # Run schema migrations if needed
-# if [ -e /var/lib/postgresql/first_run ]; then
-#     echo "Skipping schema migrations on first run..."
-#     rm /var/lib/postgresql/first_run
-# elif [ ! -z ${MC_POSTGRESQL_SKIP_MIGRATIONS+x} ]; then
-#     # Used for verifying whether ZFS backup snapshot works
-#     echo "Skipping schema migrations because 'MC_POSTGRESQL_SKIP_MIGRATIONS' is set."
-# else
-#     echo "Applying schema migrations..."
-#     /opt/mediacloud/bin/apply_migrations.sh
-#     echo "Done applying schema migrations."
-# fi
-
-# /opt/mediacloud/bin/apply_migrations.sh
+if [ -e /var/lib/postgresql/first_run ]; then
+    echo "Skipping schema migrations on first run..."
+    rm /var/lib/postgresql/first_run
+elif [ ! -z ${MC_POSTGRESQL_SKIP_MIGRATIONS+x} ]; then
+    # Used for verifying whether ZFS backup snapshot works
+    echo "Skipping schema migrations because 'MC_POSTGRESQL_SKIP_MIGRATIONS' is set."
+else
+    echo "Applying schema migrations..."
+    /opt/mediacloud/bin/apply_migrations.sh
+    echo "Done applying schema migrations."
+fi
 
 # Start PostgreSQL
 exec "${MC_POSTGRESQL_BIN_DIR}/postgres" \
